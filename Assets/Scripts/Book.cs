10db2bc9edfb97299b71a302da12f0d8b1d0c880
unknown <ondrej.richnak@gmail.com>
2021-12-02 01:35:53 +0100

    Book recepies basic

---
+++ Assets/Scripts/Book.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Book : MonoBehaviour
+{
+    public GameObject BookWithRecepis;
+    void OnMouseEnter()
+    {
+        BookWithRecepis.SetActive(true);
+    }
+
+    void OnMouseExit()
+    {
+        BookWithRecepis.SetActive(false);
+    }
+}
