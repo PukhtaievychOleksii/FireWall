8ee3d43844e629383f40a7227a1aee96ad528ed7
AlexPuch <pukhtaievych.oleksii@gmail.com>
2022-01-18 19:10:15 +0100

    IngridientPreparingShow

--- Assets/Scripts/Characters/Components/MovementComponent.cs
+++ Assets/Scripts/Characters/Components/MovementComponent.cs
@@ -5,27 +5,25 @@ using UnityEngine;
 public class MovementComponent : MonoBehaviour
 {
     public float MovingSpeed;
+    private float LerpIndex = 0.1f;
     public Vector3 Destination;
     public bool IShouldMove = false;
-    public GameObject Owner;
     void Start()
     {
-        Owner = this.gameObject;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(IShouldMove && VariablesFilled() && !PauseGame.GameIsPoused)
-        {
-            Owner.transform.position = Vector3.MoveTowards(Owner.transform.position, Destination,MovingSpeed);
-            if (Owner.transform.position == Destination) IShouldMove = false;
+        if(IShouldMove && Destination != null && !PauseGame.GameIsPoused)
+        {            
+            transform.position = Vector3.MoveTowards(transform.position,Vector3.Lerp(transform.position,Destination,LerpIndex),MovingSpeed);
+            if (transform.position == Destination)
+            {
+                IShouldMove = false;
+            }
         }
     }
 
-    private bool VariablesFilled()
-    {
-        if (Destination == null || Owner == null) return false;
-        return true;
-    }
+    
 }
